ef7aaa6f8bb24de63c581087f174948e4ac5824d
Geneticus <Geneticus0@gmail.com>
2017-09-26 03:57:06 -0700

    fixed implementation

--- Whitelist/Vrage.Game/ModAPI/MySerialOut_ModAPI.cs
+++ Whitelist/Vrage.Game/ModAPI/MySerialOut_ModAPI.cs
@@ -7,15 +7,27 @@ using VRage.ModAPI;
 using VRage.Game.ModAPI.SerialOutput;
 using VRage.Game.ModAPI;
 using Serial.IO;
+using VRage.Game.ModAPI.Ingame;
 
 namespace VRage.Game.ModAPI.MySerialOut
 {
-    public class MySerialOut 
+    public class MySerialOut :IMySerialOutAccess
     {
+        bool Ingame.IMySerialOutAccess.SendMessageToSerial(string Message, string devicename)
+        {
+            throw new NotImplementedException();
+        }
+
         bool IMySerialOutAccess.SendMessageToSerial(String Message, String devicename)
         {
             return SerialOut.SendMessageToSerial(Message, devicename);
         }
+
+        bool Ingame.IMySerialOutAccess.SendMessageToSerial(string Message, int COMPortNumber, int baudRate)
+        {
+            throw new NotImplementedException();
+        }
+
         bool IMySerialOutAccess.SendMessageToSerial(String Message, int COMPortNumber, int baudRate)
         {
             return SerialOut.SendMessageToSerial(Message, COMPortNumber, baudRate);
